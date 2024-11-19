99d440e054479f8adfad929d4c886c68551125b4
VidarDev <86915969+VidarDev@users.noreply.github.com>
2024-11-19 20:43:51 +0100

    build: :package: package installations (GSAP, Shadcn, Lenis, zustand)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  /* config options here */
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.picsum.photos",
+        port: "",
+        search: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
