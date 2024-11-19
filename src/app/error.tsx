8c9fdb76001918839b798f817532999ce70518d2
VidarDev <86915969+VidarDev@users.noreply.github.com>
2024-11-19 21:47:15 +0100

    feat: :sparkles: setting up app routing

---
+++ src/app/error.tsx
@@ -0,0 +1,5 @@
+'use client'
+
+export default function Error() {
+  return <div>No recipe has this id</div>
+}
---
+++ src/app/middleware.ts
@@ -0,0 +1,42 @@
+import { match } from '@formatjs/intl-localematcher'
+import Negotiator from 'negotiator'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+const defaultLocale = 'fr-FR'
+const locales = ['fr-FR']
+
+// Get the preferred locale, similar to the above or using a library
+function getLocale(request: NextRequest) {
+  const acceptedLanguage = request.headers.get('accept-language') ?? undefined
+  const headers = { 'accept-language': acceptedLanguage }
+  const languages = new Negotiator({ headers }).languages()
+
+  return match(languages, locales, defaultLocale)
+}
+
+export function middleware(request: NextRequest) {
+  // Check if there is any supported locale in the pathname
+  const { pathname } = request.nextUrl
+  const pathnameHasLocale = locales.some(
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`,
+  )
+
+  if (pathnameHasLocale) return
+
+  // Redirect if there is no locale
+  const locale = getLocale(request)
+  request.nextUrl.pathname = `/${locale}${pathname}`
+  // e.g. incoming request is /products
+  // The new URL is now /en-US/products
+  return NextResponse.redirect(request.nextUrl)
+}
+
+export const config = {
+  matcher: [
+    // Skip all internal paths (_next)
+    '/((?!api|_next).*)',
+    // Optional: only run on root (/) URL
+    // '/'
+  ],
+}
