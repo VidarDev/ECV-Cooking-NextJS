eb2916c2aa88c9c27cf8d581bf555d78e0b178d3
VidarDev <86915969+VidarDev@users.noreply.github.com>
2024-11-19 21:50:35 +0100

    feat: :sparkles: setting up the stores, and API functions

---
+++ src/stores/usePageTransitionStore.ts
@@ -0,0 +1,23 @@
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+
+import { PageTransitionStore } from "@/types/store";
+
+const usePageTransitionStore = create<PageTransitionStore>()(
+  devtools(
+    (set) => ({
+      isTransitionActive: false,
+      isFirstLoad: true,
+
+      setIsTransitionActive: (isTransitionActive: boolean) =>
+        set({ isTransitionActive }),
+
+      setIsFirstLoad: (isFirstLoad: boolean) => set({ isFirstLoad }),
+    }),
+    {
+      name: "page-transition-store",
+    }
+  )
+);
+
+export default usePageTransitionStore;
